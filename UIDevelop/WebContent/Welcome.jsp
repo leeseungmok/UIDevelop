965b5627e30e76671c9bc2b6649525aa5d9516d7
USER <USER@AMP-15>
2019-11-08 12:39:25 +0900

    Commit

---
+++ UIDevelop/WebContent/Welcome.jsp
@@ -0,0 +1,41 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<html>
+<head>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<title>Welcome</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand navbar-dark bg-dark">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="./Welcome.jsp">Home</a>
+				<a class="navbar-brand" href="./products.jsp">상품목록</a>
+				<a class="navbar-brand" href="./addproduct.jsp">상품등록</a>
+			</div>
+		</div>
+	</nav>
+	<%!String greeting = "웹 쇼핑몰에 오신 것을 환영합니다";
+	String tagline = "Welcome to Web Market!";%>
+	<div class="jumbotron">
+		<div class="container">
+			<h1 class="display-3">
+				<%=greeting%>
+			</h1>
+		</div>
+	</div>
+	<main role="main">
+	<div class="container">
+		<div class="text-center">
+			<h3>
+				<%=tagline%>
+			</h3>
+		</div>
+		<hr>
+	</div>
+	</main>
+	<footer class = "container">
+	<p>&copy; WebMarket</p>
+	</footer>
+</body>
+</html>
---
+++ UIDevelop/src/dao/ProductRepository.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.util.ArrayList;
+
+import dto.Product;
+
+public class ProductRepository {
+	
+	private ArrayList<Product> listOfProducts = new ArrayList<Product>();
+	private static ProductRepository instance = new ProductRepository();
+
+	public static ProductRepository getInstance(){
+		return instance;
+	} 
+
+	public ProductRepository() {
+		Product phone = new Product("P1234", "iPhone 6s", 800000);
+		phone.setDescription("4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+
+
+		listOfProducts.add(phone);
+	}
+
+	public ArrayList<Product> getAllProducts() {
+		return listOfProducts;
+	}
+	
+	public Product getProductById(String productId) {
+		Product productById = null;
+
+		for (int i = 0; i < listOfProducts.size(); i++) {
+			Product product = listOfProducts.get(i);
+			if (product != null && product.getProductId() != null && product.getProductId().equals(productId)) {
+				productById = product;
+				break;
+			}
+		}
+		return productById;
+	}
+	
+	public void addProduct(Product product) {
+		listOfProducts.add(product);
+	}
+}
